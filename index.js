c0dc519acea10dd6ae7219b9764f1a89c8b30733
Reina <r.clarkemitchell@gmail.com>
2019-11-10 18:13:06 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr){
+    let found = arr.find(function(obj){
+      return obj['result'] === "W"
+    })
+		if(found){
+      return found.year
+    } else {
+      return undefined
+    }
+}
